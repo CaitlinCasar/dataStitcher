5f2e7e0a8c9df3f9bfe8672e193fe05574cc205e
Caitlin <casar@u.northwestern.edu>
2020-03-18 15:29:04 -0500

    updating plotter

--- dataStitchR.R
+++ dataStitchR.R
@@ -5,8 +5,8 @@ print("This script was created by Caitlin Casar. DataStitchR stitches panoramic 
       x-ray energy dispersive spectroscopy.")
 
 #load dependencies 
-print("Loading script dependencies: optparse, raster, magick, tidyverse, rasterVis, randomcoloR...")
-pacman::p_load(optparse, raster, magick, tidyverse, rasterVis, randomcoloR, ggnewscale)
+print("Loading script dependencies: optparse, raster, rdgal, magick, tidyverse, rasterVis, ggnewscale...")
+pacman::p_load(optparse, raster, rdgal, magick, tidyverse, rasterVis, ggnewscale)
 print("...complete.")
 
 option_list = list(
@@ -25,7 +25,7 @@ setwd("~/Desktop/dataStitcher/example_dataset")
 
 #create list all sub-directories within main directory
 directories <- list.files(full.names = F , recursive =F)
-directories <- directories[!str_detect(directories, "Unknown|Os|SEM")]
+directories <- directories[!str_detect(directories, "Unknown|Os|SEM|.tif")]
 positions <- str_extract(dir("SEM_images", pattern = "*.tif"),  "-?\\d") 
 
 #set image coordinates
@@ -124,8 +124,16 @@ xray_brick <- do.call(brick, xray_brick_list)
 names(xray_brick) <- unlist(xray_data)
 print("...complete.")
 
+#write the brick 
+print("Writing brick...")
+sample_id <- str_extract(getwd,"([^/]+$)")
+
+x <- writeRaster(xray_brick, paste0(sample_id,'.tif'), overwrite=TRUE, format="GTiff",options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
+
+print("...complete.")
+
 #flush everything we don't need from memory
-remove(list = c("xray_brick_list", "xray_data", "empty_raster", "empty_raster_extent",
+remove(list = c("x", "xray_brick_list", "xray_data", "empty_raster", "empty_raster_extent",
                 "i", "j", "k", "path", "positions", "xy_id", "xy", 
                 "panorama_merged", "panorama", "empty_xy_id", "files", "directories"))
 
@@ -135,24 +143,47 @@ remove(list = c("xray_brick_list", "xray_data", "empty_raster", "empty_raster_ex
 
 print("Generating plot...")
 # Set color palette
-#zeroCol <-NA 
-element_colors <- distinctColorPalette(k = length(names(xray_brick)))
-names(element_colors) <- names(xray_brick)
+
+
+element_colors <- c("#FFFFFF", "#D9FFFF", "#CC80FF", "#C2FF00", "#FFB5B5", "#909090", "#3050F8",
+                    "#FF0D0D", "#90E050", "#B3E3F5", "#AB5CF2", "#8AFF00", "#BFA6A6", "#F0C8A0",
+                    "#FF8000", "#FFFF30", "#1FF01F", "#80D1E3", "#8F40D4", "#3DFF00", "#E6E6E6",
+                    "#BFC2C7", "#A6A6AB", "#8A99C7", "#9C7AC7", "#E06633", "#F090A0", "#50D050",
+                    "#C88033", "#7D80B0", "#C28F8F", "#668F8F", "#BD80E3", "#FFA100", "#A62929",
+                    "#5CB8D1", "#702EB0", "#00FF00", "#94FFFF", "#94E0E0", "#73C2C9", "#54B5B5",
+                    "#3B9E9E", "#248F8F", "#0A7D8C", "#006985", "#C0C0C0", "#FFD98F", "#A67573",
+                    "#668080", "#9E63B5", "#D47A00", "#940094", "#429EB0", "#57178F", "#00C900",
+                    "#70D4FF", "#FFFFC7", "#D9FFC7", "#C7FFC7", "#A3FFC7", "#8FFFC7", "#61FFC7",
+                    "#45FFC7", "#30FFC7", "#1FFFC7", "#00FF9C", "#00E675", "#00D452", "#00BF38",
+                    "#00AB24", "#4DC2FF", "#4DA6FF", "#2194D6", "#267DAB", "#266696", "#175487",
+                    "#D0D0E0", "#FFD123", "#B8B8D0", "#A6544D", "#575961", "#9E4FB5", "#AB5C00",
+                    "#754F45", "#428296", "#420066", "#007D00", "#70ABFA", "#00BAFF", "#00A1FF",
+                    "#008FFF", "#0080FF", "#006BFF", "#545CF2", "#785CE3", "#8A4FE3", "#A136D4",
+                    "#B31FD4", "#B31FBA", "#B30DA6", "#BD0D87", "#C70066", "#CC0059", "#D1004F",
+                    "#D90045", "#E00038", "#E6002E", "#EB0026")
+names(element_colors) <- c("H",  "He", "Li", "Be", "B",  "C",  "N",  "O",  "F",  "Ne", "Na", "Mg", "Al", "Si",
+                           "P",  "S",  "Cl", "Ar", "K",  "Ca", "Sc", "Ti", "V",  "Cr", "Mn", "Fe", "Co", "Ni",
+                           "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y",  "Zr", "Nb", "Mo",
+                           "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I",  "Xe", "Cs", "Ba",
+                           "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+                           "Lu", "Hf", "Ta", "W",  "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po",
+                           "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U",  "Np", "Pu", "Am", "Cm", "Bk", "Cf",
+                           "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt")
 
 xray_frame <- as.data.frame(xray_brick, xy=TRUE) %>%
      gather(element, value, Al:Si)
 
 ###Testing###
-test_frame <- xray_frame %>%
-  filter(element == "Al" & value!=0) 
-
-SEM_plot <- rasterVis::gplot(SEM_panorama_merged) +
-  geom_tile(aes(fill = value)) +
-  scale_fill_gradient(low = 'black', high = 'white') +
-  ggnewscale::new_scale_fill() +
-  geom_raster(test_frame, mapping = aes(x, y, fill = element, alpha = value)) +
-  scale_fill_manual(values = element_colors) +
-  ggnewscale::new_scale_fill() 
+# test_frame <- xray_frame %>%
+#   filter(element == "Al" & value!=0) 
+# 
+# SEM_plot <- rasterVis::gplot(SEM_panorama_merged) +
+#   geom_tile(aes(fill = value)) +
+#   scale_fill_gradient(low = 'black', high = 'white') +
+#   ggnewscale::new_scale_fill() +
+#   geom_raster(test_frame, mapping = aes(x, y, fill = element, alpha = value)) +
+#   scale_fill_manual(values = element_colors) +
+#   ggnewscale::new_scale_fill() 
 ###Testing end###
 
 
@@ -161,23 +192,32 @@ element_plotter<-function(coord_frame, brick, SEM_image, colors){
     geom_tile(aes(fill = value)) +
     scale_fill_gradient(low = 'black', high = 'white') +
     ggnewscale::new_scale_fill()
-  for(i in unlist(names(brick))){ 
+  for(i in names(brick)){ 
+    print(paste0("Adding ", names(brick[[i]]), " to plot..."))
     element_coords <- coord_frame %>%
       filter(element == names(brick[[i]]) & value!=0)
-    p<-p+geom_raster(element_coords, mapping = aes(x, y, fill = element, alpha = value)) +
+    p <- p+geom_raster(element_coords, mapping = aes(x, y, fill = element, alpha = value)) +
       scale_fill_manual(values = colors) +
       ggnewscale::new_scale_fill()
-    break
   }
-  suppressWarnings(print(p + coord_fixed()))
+  print("Writing plot...")
+  suppressWarnings(print(p + 
+                           coord_fixed() +
+                           guides(col = guide_legend(ncol = 3)) +
+                           theme(axis.title = element_blank(),
+                                 axis.text = element_blank(),
+                                 legend.position = "bottom",
+                                 legend.title = ggplot2::element_blank(), 
+                                 legend.text = ggplot2::element_text(size = 8),
+                                 legend.key.size = unit(0.25, "cm"))))
 }
 
-element_plotter(xray_frame, xray_brick, SEM_panorama_merged, element_colors)
-
+element_plot <- element_plotter(xray_frame, xray_brick, SEM_panorama_merged, element_colors)
+element_plot
 
 # generate PDF ------------------------------------------------------------
 
-pdf(opt$out,
+pdf("element_plot.pdf",
     width = 13.33, 
     height = 7.5)
 
@@ -191,6 +231,7 @@ print("...complete.")
 # old plotting code ----------------------------------------------------------------
 
 #level plot
+#zeroCol <-NA 
 # element_theme <- list()
 # for (i in 1:length(names(xray_brick))) {
 #   id <- which(names(element_colors) %in% names(xray_brick)[i])
@@ -206,60 +247,3 @@ print("...complete.")
 # }
 # 
 # eval(parse(text=paste0(c("SEM_plot", unlist(xray_plots)), collapse = "+")))
-
-
-
-
-
-
-
-
-
-
-# xray_frame <- as.data.frame(xray_brick, xy=TRUE) %>%
-#   gather(element, value, Al:Si)
-# 
-# rasterVis::gplot(SEM_panorama_merged) +
-#   geom_raster(aes(fill = value)) +
-#   scale_fill_gradient(low = 'black', high = 'white') +
-#   ggnewscale::new_scale_fill() +
-#   annotate(geom="raster", x=xray_frame$x, y=xray_frame$y, fill = xray_frame$element, alpha=xray_frame$value) +
-#   coord_fixed()
-# 
-# 
-# colfunc <- colorRampPalette(c(NA, "red"))
-# element_colors <- c(NA, colfunc(9))
-# 
-# plot(SEM_panorama_merged, col = gray.colors(10, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL)) 
-# #plot(xray_brick[[1]],col=alpha(element_colors, 0.4), add = T)
-# plot(xray_brick[[1]],col=NA, add = T)
-# 
-# 
-# rasterVis::gplot(xray_brick) + 
-#   geom_tile(data = xray_brick[xray_brick >0], aes(fill = value), alpha = 0.5) +
-#   scale_fill_gradient(low = NA, high = 'blue') +
-#   geom_tile(data = xray_brick[xray_brick== 0], aes(colour = NA),
-#             linetype = 0, fill = NA)
-# 
-# rasterVis::gplot(xray_brick) + 
-#   geom_tile(aes(fill = names, alpha = value))
-#   
-# 
-# 
-# 
-# element_colors <- c("r")
-# names(element_colors) <- directories
-# 
-# rasterVis::gplot(SEM_panorama_merged) +
-#   geom_tile(aes(fill = value)) +
-#   scale_fill_gradient(low = 'black', high = 'white') +
-#   ggnewscale::new_scale_fill() +
-#   geom_raster(data=xray_frame, mapping = aes(x, y, fill = element, alpha = value)) +
-#   coord_fixed()
-# 
-# 
-# levelplot(xray_brick) # plot the raster
-# 
-# bf <- writeRaster(b, filename=file.path(tmp, "multi.grd"), bandorder='BIL', overwrite=TRUE)
-# 
-# 
